b861d6fc11491cf0992314589ec1bd25acd2fd0b
Billy Gene Ridgeway <37519033+Billoncho@users.noreply.github.com>
2020-01-02 12:56:18 +0800

    Update from practice lessons

--- Eliza/src/academy/learnprogramming/Main.java
+++ Eliza/src/academy/learnprogramming/Main.java
@@ -5,16 +5,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
 	    var scanner = new Scanner(System.in);
 
 	    System.out.println(Doctor.intro());
+
 	    var userInput = "";
 
-	    while(!userInput.equalsIgnoreCase( "quit")){
+	    do {
 			userInput = scanner.nextLine();
 			String response = Doctor.response(userInput);
 			System.out.println(response);
-		}
+		} while(!userInput.equalsIgnoreCase( "quit"));
 
 		scanner.close();
     }
